5f582ee89225de7c77122867a61c5f6751d67299
Alex <alexchobanov@gmail.com>
2018-05-14 01:16:50 +0300

    Deleted unused ViewModels.

--- Source/Web/Endurance.Web.Trial.ViewModels/Manage/RemoveLoginViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Endurance.Web.Trial.ViewModels.Manage
-{
-    public class RemoveLoginViewModel
-    {
-        public string LoginProvider { get; set; }
-        public string ProviderKey { get; set; }
-    }
-}
--- Source/Web/Endurance.Web.Trial.ViewModels/Manage/ShowRecoveryCodesViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Endurance.Web.Trial.ViewModels.Manage
-{
-    public class ShowRecoveryCodesViewModel
-    {
-        public string[] RecoveryCodes { get; set; }
-    }
-}
