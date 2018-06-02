86e1f334e5fa12ba8c30e66b2323026ade7c2ede
Alex Chobanov <alexchobanov@gmail.com>
2018-06-03 00:57:02 +0300

    Trial create -  Controller, View, ViewModel https://github.com/HorseSport-achobanov/HorseSportSystem/issues/13

---
+++ Source/Web/Endurance.Web.Trial.Controllers/TrialsController.cs
@@ -0,0 +1,30 @@
+﻿namespace Endurance.Web.Trial.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using ViewModels.Trial;
+
+    public class TrialsController : BaseController
+    {
+        public TrialsController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateTrialViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Source/Web/Endurance.Web.Trial.ViewModels/Trial/CreateTrialViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace Endurance.Web.Trial.ViewModels.Trial
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Common.Mapping;
+    using Data.Trial.Models;
+
+    public class CreateTrialViewModel : IMapFrom<Trial>, IMapTo<Trial>
+    {
+        public string Name { get; set; }
+
+        public string Location { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public IList<TrialRound> Rounds { get; set; }
+    }
+}
