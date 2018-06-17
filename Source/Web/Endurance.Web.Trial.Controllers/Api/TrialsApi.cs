4c9c1210c0b31fee1c8148352a8ad9d2c8f72af2
achobanov <alexchobanov@gmail.com>
2018-06-17 14:03:36 +0300

    Fixed NuGet Packages

--- Source/Web/Endurance.Web.Trial.Controllers/Api/TrialsApi.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Endurance.Web.Trial.Controllers.Api
-{
-    using Microsoft.AspNetCore.Mvc;
-
-    [Route("api/[controller]")]
-    public class TrialsApi : ControllerBase
-    {
-    }
-}
