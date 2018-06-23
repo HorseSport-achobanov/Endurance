dae66a32450875d110fb2124603c4ee11df95b44
Alex Chobanov <alexchobanov@gmail.com>
2018-06-23 13:13:59 +0300

    Modified views and ViewModels https://github.com/HorseSport-achobanov/HorseSportSystem/issues/16

---
+++ Source/Web/Endurance.Web.Trial.ViewModels/Trial/TrialRoundPerformanceViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Endurance.Web.Trial.ViewModels.Trial
+{
+    class TrialRoundPerformanceViewModel
+    {
+    }
+}
