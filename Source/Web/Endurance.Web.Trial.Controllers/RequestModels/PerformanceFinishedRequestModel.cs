0a4812a597a17d6fda9d69be24edfca28c756ea7
Alex Chobanov <alexchobanov@gmail.com>
2018-06-23 20:40:52 +0300

    Moved project in correct directory

---
+++ Source/Web/Endurance.Web.Trial.Controllers/RequestModels/PerformanceFinishedRequestModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Endurance.Web.Trial.Controllers.RequestModels
+{
+    public class PerformanceFinishedRequestModel
+    {
+        public int Id { get; set; }
+
+        public string FinishedAtString { get; set; }
+    }
+}
