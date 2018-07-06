4ac4e6a4c1626a97b247ea1cd2280a60e3506090
Alex Chobanov <alexchobanov@gmail.com>
2018-07-06 13:51:25 +0300

    Created generic editor model for Select list

---
+++ Source/Web/Web.Common/ViewModels/SelectOption.cs
@@ -0,0 +1,17 @@
+﻿namespace Web.Common.ViewModels
+{
+    using AutoMapper;
+    using global::Common.Mapping;
+    using global::Data.Common.Models;
+    using Microsoft.AspNetCore.Mvc.Rendering;
+
+    public class SelectOption : SelectListItem, IMapFrom<BaseClub>, IHasCustomMappings
+    {
+        public void CreateMappings(IMapperConfigurationExpression configuration)
+        {
+            configuration.CreateMap<BaseClub, SelectOption>()
+                .ForMember(m => m.Value, opt => opt.MapFrom(e => e.Id))
+                .ForMember(m => m.Text, opt => opt.MapFrom(e => e.Name));
+        }
+    }
+}
