c27adff0550657d1c4b134ef05189c592fc8689b
Alex Chobanov <alexchobanov@gmail.com>
2018-07-06 14:09:11 +0300

    Implemented Create Horse flow https://github.com/HorseSport-achobanov/HorseSportSystem/issues/17

---
+++ Source/Web/Endurance.Web.Trial.Controllers/HorsesController.cs
@@ -0,0 +1,58 @@
+﻿namespace Endurance.Web.Trial.Controllers
+{
+    using Data.Trial.Models;
+    using global::Data.Common.Models;
+    using global::Services.Common.Contracts;
+    using global::Web.Common.ViewModels;
+    using Microsoft.AspNetCore.Mvc;
+    using ViewModels.Horse;
+
+    public class HorsesController : BaseController
+    {
+        private readonly IDataService<TrialHorse> horsesData;
+        private readonly IDataService<BaseClub> clubsData;
+        private readonly IAutomapperService mapper;
+
+        public HorsesController(
+            IDataService<TrialHorse> horsesData,
+            IDataService<BaseClub> clubsData,
+            IAutomapperService mapper)
+        {
+            this.horsesData = horsesData;
+            this.clubsData = clubsData;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var viewModel = PrepareClubs(new TrialHorseViewModel());
+            return View(viewModel);
+        }
+
+        public IActionResult Create(TrialHorseViewModel viewModel)
+        {
+            var club = this.clubsData.GetById(viewModel.ClubId);
+            if (club == null)
+            {
+                ModelState.AddModelError(nameof(TrialHorseViewModel.ClubId), "Club does not exist");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(PrepareClubs(viewModel));
+            }
+
+            this.horsesData.Add(this.mapper.Map<TrialHorse>(viewModel));
+
+            return RedirectToAction("Create");
+        }
+
+        private TrialHorseViewModel PrepareClubs(TrialHorseViewModel viewModel)
+        {
+            viewModel.ClubOptions = this.mapper.MapCollection<BaseClub, SelectOption>(this.clubsData.GetAll());
+
+            return viewModel;
+        }
+    }
+}
---
+++ Source/Web/Endurance.Web.Trial.ViewModels/Horse/TrialHorseViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Endurance.Web.Trial.ViewModels.Horse
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using Common.Mapping;
+    using Data.Trial.Models;
+    using global::Web.Common.ViewModels;
+
+    public class TrialHorseViewModel : IMapFrom<TrialHorse>, IMapTo<TrialHorse>
+    {
+        public string Name { get; set; }
+
+        public string Breed { get; set; }
+
+        [UIHint("SelectList")]
+        public int ClubId { get; set; }
+
+        public IList<SelectOption> ClubOptions { get; set; }
+    }
+}
